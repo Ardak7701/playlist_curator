52d6c83c653bc7f11f88490b73f20e930b647c97
Ardak7701 <251758@astanait.edu.kz>
2026-05-17 15:59:29 +0500

    ,

--- services/storage.py
+++ services/storage.py
@@ -1,7 +1,67 @@
-class Recommender:
+import json
+import csv
+import os
+
+from models.song import Song
+class StorageManager:
     @staticmethod
-    def suggest_similar(playlist, genre):
-        """Кастомный генератор для обработки данных (3 балла за Iterators/Generators)."""
-        for song in playlist.get_songs():
-            if song.genre == genre:
-                yield f"Similar to {song._title}: [New Discovery in {genre}]"
+    def save_to_json(playlist, filename="data/playlist.json"):
+        os.makedirs("data", exist_ok=True)
+
+        data = []
+
+        for song in playlist:
+            data.append({
+                "title": song._title,
+                "artist": song._artist,
+                "genre": song.genre,
+                "duration": song.duration
+            })
+
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
+
+
+    @staticmethod
+    def load_from_json(filename="data/playlist.json"):
+        songs = []
+
+        if not os.path.exists(filename):
+            return songs
+
+        with open(filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        for item in data:
+            songs.append(
+                Song(
+                    item["title"],
+                    item["artist"],
+                    item["genre"],
+                    item["duration"]
+                )
+            )
+
+        return songs
+    
+    @staticmethod
+    def export_to_csv(playlist, filename="data/playlist.csv"):
+        os.makedirs("data", exist_ok=True)
+
+        with open(filename, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+
+            writer.writerow([
+                "Title",
+                "Artist",
+                "Genre",
+                "Duration"
+            ])
+
+            for song in playlist:
+                writer.writerow([
+                    song._title,
+                    song._artist,
+                    song.genre,
+                    song.duration
+                ])
