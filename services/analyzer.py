89ecb0e4bf4b92c743fff59629ee178eb18c472b
Ardak7701 <251758@astanait.edu.kz>
2026-05-17 16:35:55 +0500

    2

--- services/analyzer.py
+++ services/analyzer.py
@@ -1,13 +1,23 @@
 from collections import Counter
 
 class GenreAnalyzer:
+    """
+    Provides playlist analysis functionality.
+    """
+
     @staticmethod
     def analyze_genres(playlist):
-        genres = list(map(lambda song: song.genre, playlist))
+        genres = list(
+            map(lambda song: song.genre, playlist)
+        )
+
         return Counter(genres)
 
     @staticmethod
     def filter_by_genre(playlist, genre):
         return list(
-            filter(lambda song: song.genre == genre, playlist)
+            filter(
+                lambda song: song.genre.lower() == genre.lower(),
+                playlist
+            )
         )
--- services/exporter.py
+++ services/exporter.py
@@ -1,16 +1,17 @@
-import re
+class PlaylistExporter:
+    """
+    Exports playlist into M3U format.
+    """
 
-class M3UExporter:
     @staticmethod
-    def export(playlist, filename):
-        # Регулярное выражение: только буквы, цифры, точки и тире (2 балла за Regex)
-        if not re.match(r'^[\w\-. ]+$', filename):
-            raise ValueError("Invalid filename! Use only letters and numbers.")
-        
-        full_path = f"{filename}.m3u"
-        with open(full_path, "w", encoding="utf-8") as f:
-            f.write("#EXTM3U\n")
+    def export_to_m3u(playlist, filename="playlist.m3u"):
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write("#EXTM3U\n")
+
             for song in playlist:
-                f.write(f"#EXTINF:{song.duration_sec},{song.get_info()}\n")
-                f.write(f"C:/Music/{song._title}.mp3\n")
-        return full_path
+                file.write(
+                    f"#EXTINF:{song.duration},"
+                    f"{song._artist} - {song._title}\n"
+                )
+
+        print(f"Playlist exported to {filename}")
