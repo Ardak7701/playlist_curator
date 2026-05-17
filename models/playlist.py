ccbdc6f0b26093566d339e831e0459d59462d356
Ardak7701 <251758@astanait.edu.kz>
2026-05-17 15:42:47 +0500

    ,

--- models/playlist.py
+++ models/playlist.py
@@ -1,20 +1,22 @@
 class Playlist:
-    """
-    Класс плейлиста (критерий Core OOP).
-    Реализует инкапсуляцию и кастомную итерацию.
-    """
     def __init__(self, name):
         self.name = name
-        self.__songs = []  # Приватный список песен (Инкапсуляция)
+        self.__songs = []
 
     def add_song(self, song):
-        """Метод для добавления объекта Song (Association)."""
         self.__songs.append(song)
 
+    def remove_song(self, title):
+        self.__songs = [
+            song for song in self.__songs
+            if song._title != title
+        ]
+
     def get_songs(self):
-        """Возвращает список песен."""
         return self.__songs
 
     def __iter__(self):
-        """Кастомный итератор (критерий Iterators)."""
         return iter(self.__songs)
+
+    def __len__(self):
+        return len(self.__songs)
